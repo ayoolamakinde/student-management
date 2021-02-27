0978cf6583ba125963c10f33424e5497fbcda026
ayoolamakinde <43256160+ayoolamakinde@users.noreply.github.com>
2021-02-27 13:34:28 -0500

    Revert "This is my second commit"

--- server.py
+++ server.py
@@ -7,12 +7,13 @@ app = connexion.App(__name__, specification_dir='./')
 # Read the swagger.yml file to configure the endpoints
 app.add_api('swagger.yml')
 
-# Create a URL route in our application for "/"
+#Create a URL route in our application for "/"
 @app.route('/')
 def home():
     """
     This function just responds to the browser ULR
     localhost:5000/
+
     :return:        the rendered template 'home.html'
     """
     return render_template('home.html')
