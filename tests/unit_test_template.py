1603180d525f5f6f073308f167dab31258fb76ba
ayoolamakinde <makindeokikiola@gmail.com>
2021-03-11 20:18:40 -0500

    updat comment

--- tests/unit_test_template.py
+++ tests/unit_test_template.py
@@ -3,8 +3,7 @@ import requests
 
 class playTest(unittest.TestCase):
 
-    #this is just to include a test in the CI/CD pipeline for the purpose of CI/CD
-    #tutoring
+    #this is just to include a test in the CI/CD pipeline for the purpose of CI/CD tutoring
     def test_home(self):
         response = requests.get("http://facebook.com")
         assert response.status_code == 200
